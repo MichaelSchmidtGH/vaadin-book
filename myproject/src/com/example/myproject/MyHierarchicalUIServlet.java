36ea0d78e0a558a526a7e6e13069a34001ad93f4
MichaelSchmidtGH <schmidt@KDED310895.csf.local>
2016-07-19 17:23:56 +0200

    Servlet für Vaadin Resourcen extrahiert.

--- myproject/src/com/example/myproject/MyHierarchicalUIServlet.java
+++ myproject/src/com/example/myproject/MyHierarchicalUIServlet.java
@@ -1,9 +1,7 @@
 package com.example.myproject;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
-import com.example.myproject.utils.Utils;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 
@@ -11,11 +9,4 @@ import com.vaadin.server.VaadinServlet;
 @VaadinServletConfiguration(productionMode = false, ui = MyHierarchicalUI.class)
 public class MyHierarchicalUIServlet extends VaadinServlet {
 	public static final long serialVersionUID = 1L;
-
-	@Override
-	protected void servletInitialized() throws ServletException {
-		super.servletInitialized();
-		Utils.log("(MyHierarchicalUIServlet servletInitialized) ");
-	}
-
 }
